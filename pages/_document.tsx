993a1b1e549607756b323b3cc737cdbec1c92c35
soleilyasmina <mounlasolomon@gmail.com>
2022-05-22 21:58:44 -0400

    update style to match original

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Abel&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Miriam+Libre:wght@400;700&display=swap"
             rel="stylesheet"
             crossOrigin="anonymous"
           />
